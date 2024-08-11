930b4bf9bf7b9f20bde6a6cdd5b02d64ea4e780b
Oderlan <oderlanfreire@gmail.com>
2024-08-11 15:47:34 -0300

    aula 11 - condições parte 1 concluído

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 12
+console.log(`A velocidade do carro é ${vel} Km/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Diriga sempre usando o cinto de segurança!`)
